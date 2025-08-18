9f0af4feb2c60e20d5b7d3ec03154d14aad17c7d
chiiipk <chi.pk235482@sis.hust.edu.vn>
2025-08-19 00:10:27 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -166,8 +166,9 @@ class TemporarilySeededRandom:
         self.stored_np_state = np.random.get_state()
 
         # Set the random seed
-        random.seed(self.seed)
+        random.seed(int(self.seed))
         np.random.seed(self.seed)
+
 
     def __exit__(self, exc_type, exc_value, traceback):
         # Restore the random state
